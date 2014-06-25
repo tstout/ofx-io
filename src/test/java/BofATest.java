7c99213ae7a2dcecc380e9775bf9d5c3395784a1
Todd Stout <todd.tstout@gmail.com>
2014-06-24 21:55:27 -0500

    update test

---
+++ src/test/java/BofATest.java
@@ -0,0 +1,62 @@
+import net.sf.ofx4j.OFXException;
+import net.sf.ofx4j.client.AccountStatement;
+import net.sf.ofx4j.client.BankAccount;
+import net.sf.ofx4j.client.FinancialInstitution;
+import net.sf.ofx4j.client.FinancialInstitutionService;
+import net.sf.ofx4j.client.context.OFXApplicationContextHolder;
+import net.sf.ofx4j.client.impl.FinancialInstitutionServiceImpl;
+import net.sf.ofx4j.domain.data.banking.AccountType;
+import net.sf.ofx4j.domain.data.banking.BankAccountDetails;
+import net.sf.ofx4j.domain.data.common.Transaction;
+import ofx.client.BoaData;
+import ofx.client.Credentials;
+import org.junit.Test;
+
+import java.util.Date;
+
+import static com.google.common.base.Throwables.*;
+import static ofx.client.Dates.*;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+public class BofATest {
+    @Test public void somethest() {
+        BoaData data = new BoaData();
+
+        OFXApplicationContextHolder.setCurrentContext(data.appContext());
+
+        FinancialInstitutionService service
+                = new FinancialInstitutionServiceImpl();
+
+        FinancialInstitution fi = service.getFinancialInstitution(data);
+
+        Credentials creds = Credentials.fromProperties(".boa-creds.properties");
+
+        BankAccountDetails details = new BankAccountDetails();
+        details.setRoutingNumber(creds.routing());
+        details.setAccountNumber(creds.account());
+        details.setAccountType(AccountType.CHECKING);
+
+        BankAccount bankAccount
+                = fi.loadBankAccount(details,
+                creds.user(),
+                creds.pass());
+
+        Date startDate = newDate("2014/05/24");
+        Date endDate = newDate("2014/05/27");
+
+        try {
+            AccountStatement statement
+                    = bankAccount.readStatement(startDate, endDate);
+            assertThat(statement.getTransactionList().getTransactions().size(), not(0));
+
+            for (Transaction tran : statement.getTransactionList().getTransactions()) {
+                System.out.println(tran);
+            }
+        } catch (OFXException e) {
+            propagate(e);
+        }
+
+
+    }
+}
